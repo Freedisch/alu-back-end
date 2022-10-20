e8dc7967f49a4fd95d0a4f8faeb07a07bdb2a0cc
Freedisch <freeproduc@gmail.com>
2022-10-20 19:33:51 +0200

    awesome

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+""" hope to get ride of the validation system """
+
 
 from urllib import response
 import requests
 import sys
 
 if __name__=="__main__":
+    """ DOne """
+    
     response = requests.get(sys.argv[1])
     if(response.status_code > 400):{
         print("Error code: {}".format(response.status_code))
