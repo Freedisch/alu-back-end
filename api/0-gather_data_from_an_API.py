377999d126b2303c087d5bc03da01944f14fbb60
Freedisch <freeproduc@gmail.com>
2022-10-20 23:41:47 +0200

    implemented get requests

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """ hope to get ride of the validation system """
 
-
-from urllib import response
 import requests
 import sys
 
 if __name__=="__main__":
-    """ DOne """
-    
-    response = requests.get(sys.argv[1])
-    if(response.status_code > 400):{
-        print("Error code: {}".format(response.status_code))
-    }
-    else:
-        print("Employee {response.content.EMPLOYEE_NAME} is done with tasks({response.content.NUMBER_OF_DONE_TASKS}/{response.content.TOTAL_NUMBER_OF_TASKS}):")
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}" \
+        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(sys.argv[1])).json()
+
+    tasks = 0
+    taskcompleted = []
+    for task in todo:
+        
+        if todo['userId'] == sys.argv[1]:
+            tasks += 1
+        
+            if todo['completed']:
+                taskcompleted.append(todo['title']);
+
+    name = user['name']
+    print("Employee {} is done with tasks({}/{}):".format(name, \
+        len(taskcompleted), tasks))
+
+    for task in taskcompleted:
+        print("\t {}", format(task))    
