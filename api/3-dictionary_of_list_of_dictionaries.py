075ed9eea2363ce7b6eead17708280364f5c02ae
Freedisch <freeproduc@gmail.com>
2022-10-21 15:54:41 +0200

    Records all tasks from all employees

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""validation"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    content = []
+    task_list = {}
+    for task in todo:
+        _id = task["userId"]
+        user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(_id)).json()
+        task_list[_id] = []
+        task_list[_id].append({
+            "username": user['username'],
+            "task": task["title"],
+            "completed": task["completed"]
+        })
+    with open("todo_all_employees.json", 'w') as file:
+        json.dump(task_list, file)
