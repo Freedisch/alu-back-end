b077c13daa66853a4e027e8758a0ed5f1ced4268
Freedisch <freeproduc@gmail.com>
2022-10-21 10:54:12 +0200

    solved typeError

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,24 +6,23 @@ import requests
 import sys
 
 
-if __name__=="__main__":
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}" \
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"\
         .format(sys.argv[1])).json()
     todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/" \
         .format(sys.argv[1])).json()
     content = []
 
     for task in todo:
-        if task['userId'] == sys.argv[1]:
-            content.append([str(sys.argv[1]),
-            user["username"],
-            todo["completed"],
-            todo["title"]])
-        
+        content.append([str(sys.argv[1]),
+                        user["username"],
+                        task["completed"],
+                        task["title"]])
+
     csv_file = "{}.csv".format(sys.argv[1])
-    with open(csv_file, 'w', newline='') as csv:
-        write = csv.writer(csv, quoting=csv.QUOTE_ALL)
-        for row in content:
-            for item in row:
-                str(item)
-            write.writerow(row)
+    with open(str(sys.argv[1]) + '.csv', "w") as csv_file:
+        for item in content:
+            csv_file.write('"' + str(sys.argv[1]) + '",' +
+                        '"' + item[1] + '",' +
+                        '"' + str(item[2]) + '",' +
+                        '"' + item[3] + '",' + "\n")
