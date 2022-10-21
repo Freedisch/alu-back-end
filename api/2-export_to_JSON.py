0f4f323d35607cbb7b341f284f7d23c71a3f67d6
Freedisch <freeproduc@gmail.com>
2022-10-21 15:20:45 +0200

    export to json

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""validation"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/"
+                        .format(sys.argv[1])).json()
+    content = []
+    for task in todo:
+        content.append(
+            {
+                "task": task['title'],
+                "completed": task['completed'],
+                "username": user['username']
+            }
+        )
+    file_name = "{}.json".format(sys.argv[1])
+    with open(file_name, 'w') as file:
+        json.dump(content, file)
