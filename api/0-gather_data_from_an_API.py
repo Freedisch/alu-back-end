eca7aea1eb9bc3ff516ba40930a01a27b62e9744
Freedisch <freeproduc@gmail.com>
2022-10-21 09:32:15 +0200

    fix pep8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,25 @@ import sys
 
 """Too tired"""
 
-if __name__=="__main__":
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}" \
-        .format(sys.argv[1])).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(sys.argv[1])).json()
+if __name__ == "__main__":
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(sys.argv[1])).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos/"
+                        .format(sys.argv[1])).json()
 
     tasks = 0
     taskcompleted = []
     for task in todo:
-        
-        if task['userId'] == int(sys.argv[1]):
+
+        if task['userId'] == sys.argv[1]:
             tasks += 1
-        
+
             if task['completed']:
-                taskcompleted.append(task['title']);
+                taskcompleted.append(task['title'])
 
     name = user['name']
-    print("Employee {} is done with tasks({}/{}):".format(name, \
-        len(taskcompleted), tasks))
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          len(taskcompleted), tasks))
 
     for task in taskcompleted:
-        print("\t {}", format(task))    
+        print("\t {}", format(task))
