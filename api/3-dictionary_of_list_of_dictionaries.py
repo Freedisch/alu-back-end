27d9fb5a9b7b840de78c14aced85f7b7636761bb
Freedisch <freeproduc@gmail.com>
2022-10-21 15:55:02 +0200

    Fixed PEP8 error

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -7,13 +7,14 @@ import sys
 
 
 if __name__ == "__main__":
-    
+
     todo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
     content = []
     task_list = {}
     for task in todo:
         _id = task["userId"]
-        user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(_id)).json()
+        user = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".format(_id)).json()
         task_list[_id] = []
         task_list[_id].append({
             "username": user['username'],
